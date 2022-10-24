fc300c44b3d8f2862e5b7726af003315dab5284d
Mathieu Moran <titmatt94@gmail.com>
2022-10-24 13:03:53 -0400

    Update AngelThump2VLC.py

--- AngelThump2VLC.py
+++ AngelThump2VLC.py
@@ -8,15 +8,15 @@ parser.add_argument("-c", help = "AngelThump channel name")
 args = parser.parse_args()
 
 def getToken():
-        print("[+] getting token")
-        endpoint = 'https://vigor.angelthump.com/' + args.c + '/token'
-        jHeaders =  {"Content-Type": "application/json", "Identifier": "SwnpX0RnA99YdRj0SPqs"}
-        r = post(endpoint, headers=jHeaders)
-        return json.loads(r.text)["token"]
+    print("[+] getting token")
+    endpoint = 'https://vigor.angelthump.com/' + args.c + '/token'
+    jHeaders =  {"Content-Type": "application/json", "Identifier": "SwnpX0RnA99YdRj0SPqs"}
+    r = post(endpoint, headers=jHeaders)
+    return json.loads(r.text)["token"]
 
 def getHLS():
-        endpoint = 'https://vigor.angelthump.com/hls/' + args.c + '.m3u8?token=' + getToken()
-        return endpoint
+    endpoint = 'https://vigor.angelthump.com/hls/' + args.c + '.m3u8?token=' + getToken()
+    return endpoint
 
 def playVLC():
     subprocess.Popen(["C:/Program Files/VideoLAN/VLC/vlc.exe",getHLS()])
